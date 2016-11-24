c306c67451b7352a9be9534f38190b4baf114d03
Cryru <TheCryru@gmail.com>
2016-11-24 21:30:41 +0200

    General Update - Conflict Fix

---
+++ www/scripts/Controllers/registerController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Lucifer on 24-Nov-16.
+ */
+$('#buttonRegisterUser').click(register);
+function register (){
+    let username = $('.username').val();
+    let password = $('.passwd').val();
+    kinvey.Register(username,password,successRegister,errorRegister);
+}
+function successRegister() {
+    //TODO
+}
+function errorRegister(){
+    //TODO
+}
