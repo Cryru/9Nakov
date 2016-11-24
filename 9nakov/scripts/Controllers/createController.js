d03f183ef308e7c1dc6fabf99b6fd4821caa600b
fiher <fihercho@gmail.com>
2016-11-24 20:11:39 +0200

    BugCreating2

--- 9nakov/scripts/Controllers/createController.js
+++ 9nakov/scripts/Controllers/createController.js
@@ -3,18 +3,15 @@
  */
      // Should remove later
     // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-     const appKey = "kid_SyCmuImMl";
-     const appSecret = "f55fee035573491d8a4f32e3a11f3bc4";
-     let kinvey = new Kinvey(appKey,appSecret);
-     kinvey.Login("Admin","Admin",successPost,errorPost);
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-   // Никва идея защо не работи тва ---- > $('.postButton').click(createPost());
+const appKey = "kid_SyCmuImMl";
+const appSecret = "f55fee035573491d8a4f32e3a11f3bc4";
+let kinvey = new Kinvey(appKey,appSecret);
+kinvey.Login("Admin","Admin",successPost,errorPost);
+// Никва идея защо не работи тва ---- > $('.postButton').click(createPost());
 
 function createPost(){
 console.log("aye ei");
-    let picture = JSON.stringify($('#picture')[0].files[0]);
     let title = $('.title').val();
-
     let post = {
         title:title,
         picture:picture
