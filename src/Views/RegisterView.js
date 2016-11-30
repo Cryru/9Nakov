2a6c4764ec97a1922ae23887cbc40e16fc63d11b
zondario <ivo.cenov1@gmail.com>
2016-11-30 14:44:36 +0200

    Register and posts styled

--- src/Views/RegisterView.js
+++ src/Views/RegisterView.js
@@ -4,12 +4,13 @@ import './Styles/RegisterView.css';
 export default class RegisterView extends Component {
   render() {
       return <div>
-    				<h1>Register</h1>
+
     					<form id="formRegister" onSubmit={this.props.registerEvent}>
+							<h1>Register</h1>
     						<input name="Username" type="text" id="regUsername" placeholder="Username" required />
     						<input name="Password" type="password" id="regPassword" placeholder="Password" required />
     						<input name="PasswordConfirm" type="password" id="regPasswordConf" placeholder="Password Confirm" required />
-    						<input type="submit" value="Register" />
+    						<input type="submit" value="Register" /><br/>
     						Already registered? <a>Click here to login.</a>
     					</form>
     				</div>
