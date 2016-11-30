a8200adbfd8b26c36cddd596b86e092276a7a2e3
zondario <ivo.cenov1@gmail.com>
2016-11-30 14:21:48 +0200

    Login form styled

--- src/Views/LoginView.js
+++ src/Views/LoginView.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './Styles/LoginView.css';
 
 export default class LoginView extends Component {
-  render() {
-      return <div>
-    					<h1>Login</h1>
-    					<form id="formLogin" onSubmit={this.props.loginEvent}>
-    						<input name="Username" type="text" id="loginUsername" placeholder="Username" required />
-    						<input name="Password" type="password" id="loginPassword" placeholder="Password" required />
-    						<input type="submit" value="Login"/>
-    						Not registered? <a>Click here to register.</a>
-    					</form>
-    				</div>
-  }
+    render() {
+        return <div>
+
+			<form id="formLogin" onSubmit={this.props.loginEvent}>
+				<h1>Login</h1>
+				<input name="Username" type="text" id="loginUsername" placeholder="Username" required />
+				<input name="Password" type="password" id="loginPassword" placeholder="Password" required />
+				<input type="submit" value="Login"/><br/>
+				Not registered? <a>Click here to register.</a>
+			</form>
+		</div>
+    }
 }
