2df7bab84ebce54a28434fb0d099a40de9e9df64
fiher <fihercho@gmail.com>
2016-12-07 12:42:12 +0200

    CreateView modified

--- src/Views/CreateView.js
+++ src/Views/CreateView.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './Styles/RegisterView.css';
-
+import './Styles/CreateView.css';
 export default class CreateView extends Component {
   render() {
       return <div>
-    					<h1>Post</h1>
+    					<h1 id="createHeader">Post</h1>
     					<form id="formCreate" onSubmit={this.props.createEvent}>
+							<label id="addTitle">AddTitle</label>
+							<br></br>
     						<input name="Title" type="text" id="creTitle" placeholder="Title" required />
-    						<label>Select an image:</label>
+							<br></br>
+    						<label id="selectImage">Select an image:</label>
+							<br></br>
     						<input name="File" type="file" id="creFile" accept="image/*" required />
-    						<label>Maximum: 9MB</label>
+							<br></br>
+    						<label id="maximumSize">Maximum: 9MB</label>
     						<input type="submit" value="Post!" />
     					</form>
     				</div>
